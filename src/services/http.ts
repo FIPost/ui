98ed8a9c08748254a557d3fae863ea8ae8d1e66c
Aron Heesakkers <aron.heesakkers@via.nl>
2021-05-14 14:02:40 +0200

    Removed timeout

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,18 +1,8 @@
-import axios, { AxiosRequestConfig } from 'axios';
+import axios from 'axios';
 
-var http = axios.create({
+export default axios.create({
   baseURL: process.env.VUE_APP_API_GATEWAY,
   params: {
     // API params go here
   },
 });
-
-http.interceptors.request.use((config: AxiosRequestConfig) : AxiosRequestConfig => {
-  setTimeout(() => {
-    return config;
-  }, 3000);
-  return config;
-});
-
-
-export default http;
